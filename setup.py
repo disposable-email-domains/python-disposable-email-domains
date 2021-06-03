7e4ce44f07541ccbbdbb44344ffb216a8cefe99b
Dustin Ingram <di@users.noreply.github.com>
2021-06-03 17:18:00 -0500

    Update repos (#73)

--- setup.py
+++ setup.py
@@ -4,37 +4,37 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = '0.0.64'
+__version__ = "0.0.64"
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='disposable-email-domains',
+    name="disposable-email-domains",
     version=__version__,
-    description='A set of disposable email domains',
+    description="A set of disposable email domains",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/di/disposable-email-domains',
-    author='Dustin Ingram',
-    author_email='github@dustingram.com',
-    license='MIT',
+    url="https://github.com/disposable-email-domains/disposable-email-domains",
+    author="Dustin Ingram",
+    author_email="github@dustingram.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='disposable email domains blocklist',
-    packages=['disposable_email_domains'],
+    keywords="disposable email domains blocklist",
+    packages=["disposable_email_domains"],
     extras_require={
-        'dev': ['check-manifest'],
+        "dev": ["check-manifest"],
     },
 )
