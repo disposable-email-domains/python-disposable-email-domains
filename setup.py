cd2a970db92014f988f23accb13f01a1afe7ceeb
Dustin Ingram <di@users.noreply.github.com>
2021-06-08 18:44:17 -0400

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="disposable email domains blocklist",
     packages=["disposable_email_domains"],
