c345916b68bc403fc924c2f31d4e91ecc8d2fdc1
Dee Eye <dee-eye@users.noreply.github.com>
2022-01-19 00:34:44 +0000

    Version 0.0.71

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = "0.0.70"
+__version__ = "0.0.71"
 
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
