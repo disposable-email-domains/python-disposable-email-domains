c7b033c43dbe18a978a9990d88dd0da6741e6d32
Dustin Ingram <di@users.noreply.github.com>
2025-01-07 14:49:27 +0000

    Add support for 3.12

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Typing :: Typed",
     ],
     keywords="disposable email domains blocklist",
